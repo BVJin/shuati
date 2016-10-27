81fc1738e03d2b80ffdc16cb158c0dd499c71124
wsq3086 <wsq3086@bu.edu>
2016-10-27 17:27:31 -0400

    123
    wtf is dis

--- leetcode-001/src/Siqi.java
+++ leetcode-001/src/Siqi.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+
 public class Siqi {
     public int[] twoSum(int[] nums, int target) {
-        return null;
+    	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+    	int[] result = new int[2];
+    	
+    	for(int i = 0; i < nums.length; i++){
+    		if(map.get(target - nums[i]) != null){
+    			result[1] = i;
+    			result[0] = map.get(target - nums[i]);
+    			break;
+    		}
+    		else{
+    			map.put(nums[i], i);
+    		}
+    	}
+       return result; 
     }
 }
--- leetcode-001/src/Test.java
+++ leetcode-001/src/Test.java
@@ -2,10 +2,11 @@
 
 public class Test {
 	public static void main(String args[]){
-		BVjin bv = new BVjin();
+		//BVjin bv = new BVjin();
+		Siqi si =  new Siqi();
 		int[] a = {2,7,11,15};
 		int target = 18;
-		int[] result = bv.twoSum(a, target);
+		int[] result = si.twoSum(a, target);
 		for(int i : result){
 			System.out.print(i+" ");
 		}
