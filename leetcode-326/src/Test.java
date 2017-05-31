8a6fcf740729ff29cd1dd6eadc39e9cbbe944680
BVJin <jsanchuan@gmail.com>
2017-05-30 23:47:51 -0500

    update 326

---
+++ leetcode-326/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String args[]){
+		Solution s = new Solution();
+		int[] arr = new int[]{-2,1,-1,5,10};
+		System.out.println(s.maxSubArrayLen(arr, 5));
+	}
+}
